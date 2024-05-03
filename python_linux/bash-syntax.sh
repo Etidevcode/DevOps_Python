93e9e3d0a86df48861b989a1bb3d39d8f70a686f
Etidevcode <etixicode@gmail.com>
2024-05-03 12:43:22 +0200

    Python Linux

---
+++ python_linux/bash-syntax.sh
@@ -0,0 +1,22 @@
+#
+# Copyright (c) - All Rights Reserved.
+#
+
+#!/bin/bash
+
+x=0
+
+echo "Learning Indentation"
+echo
+
+if [ $x -eq 0 ]
+then
+    echo "In the If Block."
+    echo "Value of x is 0"
+else
+    echo "In the else block."
+    echo "Value of x is non zero"
+fi
+
+echo
+echo "This statement is out of if/else block."
---
+++ python_linux/first-python-code.py
@@ -0,0 +1,11 @@
+#
+# Copyright (c) - All Rights Reserved.
+#
+
+#!/usr/bin/python
+
+print("My first Python Script.")
+print(" ")
+print("I am excited to learn python.")
+
+
